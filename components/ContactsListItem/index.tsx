cec8f7d256353151b83a94d38189a4180903991d
griffincampbell6 <griffincampbell6@gmail.com>
2021-04-30 13:46:15 -0400

    create chat room in backend

--- components/ContactsListItem/index.tsx
+++ components/ContactsListItem/index.tsx
@@ -6,6 +6,10 @@ import { User } from "../../types"
 import styles from './styles';
 import { useNavigation } from '@react-navigation/native';
 
+import { API, graphqlOperation } from "aws-amplify";
+
+import { createChatRoom, createChatRoomUser } from '../../src/graphql/mutations';
+
 export type ContactsListItemProps = {
     user: User;
 }
@@ -16,8 +20,62 @@ const ContactsListItem = (props: ContactsListItemProps) => {
 
     const navigation = useNavigation();
 
-    const onClick = () => {
-        
+    const onClick = async () => {
+        try {
+
+            // 1. Create new Chat Room
+            const newChatRoomData = await API.graphql(
+                graphqlOperation(
+                    createChatRoom, {
+                        input: {  }
+                    }
+                )
+            )
+
+            if (!newChatRoomData.data) {
+                console.log("Failed to create a chat room");
+                return;
+            }
+
+            const newChatRoom = newChatRoomData.data.createChatRoom;
+
+            console.log(newChatRoom);
+
+            // 2. Add 'user' to the Chat Room
+            await API.graphql(
+                graphqlOperation(
+                    createChatRoomUser, {
+                        input: {
+                            userID: user.id,
+                            chatRoomID: newChatRoom.id 
+                        }
+                        
+                    }
+                )
+            )
+            
+            // 3. Add authenticated user to the Chat Room
+
+            await API.graphql(
+                graphqlOperation(
+                    createChatRoomUser, {
+                        input: {
+                            userID: "1",
+                            chatRoomID: newChatRoom.id 
+                        }
+                        
+                    }
+                )
+            )
+
+            navigation.navigate('ChatRoom', {
+                id: newChatRoom.id,
+                name: "sdasda",
+            })
+
+        } catch(e) {
+            console.log(e);
+        }
     }
 
     return (
